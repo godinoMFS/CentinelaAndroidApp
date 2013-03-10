2ce49cd26b3977b630648d9c5ed783f8bd6b69e9
godinoMFS <godino.mfs.vectra@gmail.com>
2013-03-10 21:22:23 +0100

    domingo 10/03

--- asteroides/src/com/example/asteroides/Asteroides.java
+++ asteroides/src/com/example/asteroides/Asteroides.java
@@ -4,6 +4,8 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
@@ -35,16 +37,30 @@ public class Asteroides extends Activity {
 		});
 	}
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.asteroides, menu);
-		return true;
-	}
-
 	public void lanzarAcercaDe(View view) {
 		Intent i = new Intent(this, AcercaDe.class);
 		startActivity(i);
+	}
+
+	public boolean onCreateOptionMenu(Menu menu) {
+		super.onCreateOptionsMenu(menu);
+		MenuInflater inflater = getMenuInflater();
+		inflater.inflate(R.menu.asteroides, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		switch (item.getItemId()) {
+		case R.id.acercaDe:
+			lanzarAcercaDe(null);
+			break;
+		case R.id.config:
+			Intent i = new Intent(this, Preferencias.class);
+			startActivity(i);
+			break;
+		}
+		return true;
 	}
 
 }
