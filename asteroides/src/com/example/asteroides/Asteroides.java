52a7b56456b44a96800c77d1dfd107c9ee2911a6
godinoMFS <godino.mfs.vectra@gmail.com>
2013-03-10 12:27:52 +0100

    acerca de..

--- asteroides/src/com/example/asteroides/Asteroides.java
+++ asteroides/src/com/example/asteroides/Asteroides.java
@@ -1,15 +1,38 @@
 package com.example.asteroides;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
 public class Asteroides extends Activity {
+
+	private Button bAcercaDe;
+	private Button bSalir;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_asteroides);
+		bAcercaDe = (Button) findViewById(R.id.Button03);
+		bAcercaDe.setOnClickListener(new OnClickListener() {
+
+			public void onClick(View view) {
+				lanzarAcercaDe(null);
+
+			}
+		});
+		bSalir = (Button) findViewById(R.id.Button04);
+		bSalir.setOnClickListener(new OnClickListener() {
+
+			public void onClick(View view) {
+				finish();
+
+			}
+		});
 	}
 
 	@Override
@@ -17,6 +40,11 @@ public class Asteroides extends Activity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.asteroides, menu);
 		return true;
+	}
+
+	public void lanzarAcercaDe(View view) {
+		Intent i = new Intent(this, AcercaDe.class);
+		startActivity(i);
 	}
 
 }
